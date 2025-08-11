447979dfe82dfb3a306b1494d27758d3cdf11408
Gitanshu Cheema <81610178+cloydotscheema@users.noreply.github.com>
2025-08-11 23:40:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (31.69%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int i = (int)s.size() - 1;
-        while (i >= 0 && s[i] == ' ') i--;
-        int len = 0;
+        int i = s.size() - 1, len = 0;
+        
+        while (i >= 0 && s[i] == ' ') --i;
+        
         while (i >= 0 && s[i] != ' ') {
-            len++;
-            i--;
+            ++len;
+            --i;
         }
+        
         return len;
     }
 };
